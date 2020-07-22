2a04ca6ea3ec803bde12e851999ce46ab6f0082a
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-07-21 20:54:51 -0500

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     });
 
     //search button enter key feature. NOT WORKING***
-    $(#search-button).keypress(function(event)) {
+    $("#search-button").keypress(function(event)) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         // var searchTerm = $("#search-value").val();
         if (keycode === 13) {
